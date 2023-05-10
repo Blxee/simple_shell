f3640d98a3dc5cbb7b1525d67a27c0dd999b96a7
Blxee <aminedev57@gmail.com>
2023-05-10 15:22:48 +0100

    Added: some files as template (main.c, main.h, compile.sh)

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - simple shell program
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * @envp: environment variables vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[], char *envp[])
+{
+
+	return (0);
+}
