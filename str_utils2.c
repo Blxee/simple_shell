da24ab7e46a819cb3abcefeff324b8e1bb336a5b
Sokayna <sokayna.bennasseur@gmail.com>
2023-05-25 12:23:02 -0700

    merging

--- str_utils2.c
+++ str_utils2.c
@@ -125,24 +125,3 @@ char *int_to_str(long n)
 	str[i] = '\0';
 	return (str);
 }
-/**
- * _strdup - Duplicates a string
- * @str: A pointer of character
- * Return: A pointer to a string
- */
-char *_strdup(char *str)
-{
-	char *s;
-	unsigned int i;
-
-	if (str == NULL)
-		return (NULL);
-	s = malloc(sizeof(char) * (_strlen(str) + 1));
-	if (s == NULL)
-		return (0);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		s[i] = str[i];
-	}
-	return (s);
-}
